62094827edc209216492f4f677e9ab1b448c5c95
wongee93 <jhwon9304@gmail.com>
2022-10-05 23:08:51 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import { graphql } from "@octokit/graphql";
+import { useState, useEffect } from "react";
 
 function App() {
+
+  let [discussion, setDiscussion] = useState([]);
+
+  async function agoradiscussion(){
+    
+    const { repository } = await graphql(
+      `
+        {
+          repository(owner: "codestates-seb", name: "agora-states-fe") {
+            discussions(last: 15) {
+              edges {
+                node {
+                  title
+                  createdAt
+                  id
+                  author {
+                    avatarUrl
+                    login
+                  }
+                  answer {
+                    author {
+                      login
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      `,
+      {
+        headers: {
+          authorization: `token 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`,
+        },
+      }
+    );
+    return repository;
+  }
+
+  useEffect(() => {
+    agoradiscussion()
+    .then(res => {
+      console.log(res.discussions.edges);
+      setDiscussion(res.discussions.edges);
+    })
+  },[])
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div>
+      <div>{discussion.map((el, idx)=>{
+        return (<ul key={idx}>
+          <li>Title : {el.node.title}</li>
+          <li>ID : {el.node.author.login} / Date : {el.node.createdAt}</li>
+          <li>Answer : {el.node.answer !== null? <span>O</span> : <span>X</span>}</li>
+        </ul>)
+      })}</div>
+    </div>      
       </header>
     </div>
   );
